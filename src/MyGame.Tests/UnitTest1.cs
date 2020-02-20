cc6bff8ba22f023a55d04c8776988fb21be9cdec
Andi Péter <andipeti2000@gmail.com>
2020-02-20 21:26:55 +0100

    Added CI test project

---
+++ src/MyGame.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System.Runtime.InteropServices;
+using System;
+using Xunit;
+
+namespace MyGame.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true, "tru dat");
+        }
+    }
+}

106fa77ebd83a189f57af80dfbb62118e1f692ca
Gyimesi Kristóf <jzczgi@inf.elte.hu>
2020-03-11 11:10:53 +0100

    Added merge conflict

--- src/SpaceVsInvaders/Model/Model.cs
+++ src/SpaceVsInvaders/Model/Model.cs
@@ -2,10 +2,12 @@ namespace SpaceVsInvaders.Model
 {
     public class SVsIModel
     {
-        public int TickCount { get; set; }
+        public int TickCount { get; private set; }
+
+        public int N { get; private set; }
         SVsIModel()
         {
-
+            TickCount = 0;
         }
     }
 }

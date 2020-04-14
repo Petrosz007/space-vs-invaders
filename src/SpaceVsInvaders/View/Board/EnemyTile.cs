3575a7303faa4491d0ff663faddd0ed3079318d0
Andi Péter <andipeti2000@gmail.com>
2020-04-14 15:29:25 +0200

    Added HP bars

--- src/SpaceVsInvaders/View/Board/EnemyTile.cs
+++ src/SpaceVsInvaders/View/Board/EnemyTile.cs
@@ -9,6 +9,7 @@ namespace SpaceVsInvaders.View.Boards
 {
     public class EnemyTile : Tile
     {
+        private enum EnemyPlace { LEFT, CENTER, RIGHT };
         private  List<(EnemyType, int)> enemies;
         private int currHealth;
         private int maxHealth;
@@ -30,56 +31,117 @@ namespace SpaceVsInvaders.View.Boards
 
         public override void Draw(SpriteBatch spriteBatch)
         {
-            int originalSize = (width > height) ? height : width;
-            // double offsetScale = 3/5;
-            int offsetAmount = 30;
+            DrawHpBar(spriteBatch);
 
             if(enemies.Count >= 3)
             {
-                int size = originalSize * 3/5;
-                var texture = enemies[2].Item1.GetTexture();
-                var rect = new Rectangle((int)position.X + (width - size)/2 - offsetAmount, (int)position.Y + (height - size)/2, size, size);
-                spriteBatch.Draw(texture, rect, new Rectangle(0, 0, texture.Width, texture.Height), Color.White);
-
-                if(enemies[2].Item2 > 1)
-                {
-                    string count = enemies[2].Item2.ToString();
-                    var countMeasure = font.MeasureString(count);
-                    spriteBatch.DrawString(font, count, new Vector2(position.X + 4 + 10, position.Y), Color.White);
-                }
+                DrawEnemy(spriteBatch, enemies[2], EnemyPlace.LEFT);
             }
             if(enemies.Count >= 2)
             {
-                int size = originalSize * 3/5;
-                var texture = enemies[1].Item1.GetTexture();
-                var rect = new Rectangle((int)position.X + (width - size)/2 + offsetAmount, (int)position.Y + (height - size)/2, size, size);
-                spriteBatch.Draw(texture, rect, new Rectangle(0, 0, texture.Width, texture.Height), Color.White);
-
-                if(enemies[1].Item2 > 1)
-                {
-                    string count = enemies[1].Item2.ToString();
-                    var countMeasure = font.MeasureString(count);
-                    spriteBatch.DrawString(font, count, new Vector2(position.X + width - countMeasure.Y - 10, position.Y), Color.White);
-                }
+                DrawEnemy(spriteBatch, enemies[1], EnemyPlace.RIGHT);
             }
             if(enemies.Count >= 1)
             {
-                int size = originalSize;
-                var texture = enemies[0].Item1.GetTexture();
-                var rect = new Rectangle((int)position.X + (width - size)/2, (int)position.Y + (height - size)/2, size, size);
-                spriteBatch.Draw(texture, rect, new Rectangle(0, 0, texture.Width, texture.Height), Color.White);
-
-                if(enemies[0].Item2 > 1)
-                {
-                    string count = enemies[0].Item2.ToString();
-                    var countMeasure = font.MeasureString(count);
-                    spriteBatch.DrawString(font, count, new Vector2(position.X + 4 + (width - countMeasure.Y)/2, position.Y), Color.White);
-                }
+                DrawEnemy(spriteBatch, enemies[0], EnemyPlace.CENTER);
             }
-            
-            spriteBatch.DrawString(font, $"{currHealth}/{maxHealth}", new Vector2(position.X + 4, position.Y + height - 20), Color.White);
 
             base.Draw(spriteBatch);
+        }
+
+        private void DrawEnemy(SpriteBatch spriteBatch, (EnemyType, int) enemy, EnemyPlace enemyPlace)
+        {  
+            int originalSize = (width > height) ? height : width;
+
+            (int size, int offsetAmount) = enemyPlace switch {
+                EnemyPlace.LEFT   => (originalSize * 3/5, -30),
+                EnemyPlace.CENTER => (originalSize,         0),
+                EnemyPlace.RIGHT  => (originalSize * 3/5,  30),
+            };
+
+
+            var texture = enemy.Item1.GetTexture();
+            var rect = new Rectangle((int)position.X + (width - size)/2 + offsetAmount, (int)position.Y + (height - size)/2, size, size);
+            spriteBatch.Draw(texture, rect, new Rectangle(0, 0, texture.Width, texture.Height), Color.White);
+
+            if(enemy.Item2 > 1)
+            {
+                string count = enemy.Item2.ToString();
+                var countMeasure = font.MeasureString(count);
+                var textX = enemyPlace switch {
+                    EnemyPlace.LEFT   => 4 + width/4 - countMeasure.X/2,
+                    EnemyPlace.CENTER => (width - countMeasure.X)/2,
+                    EnemyPlace.RIGHT  => width - width/4 - countMeasure.X/2 - 4,
+                };
+
+                var pos = new Vector2(position.X + textX, position.Y + height - 4 - countMeasure.Y);
+                DrawOutlinedString(spriteBatch, font, count, pos, Color.White);
+            }
+        }
+
+        private void DrawOutlinedString(SpriteBatch spriteBatch, SpriteFont font, string text, Vector2 pos, Color color)
+        {
+                spriteBatch.DrawString(font, text, pos + new Vector2(1,1), Color.Black);
+                spriteBatch.DrawString(font, text, pos + new Vector2(1,-1), Color.Black);
+                spriteBatch.DrawString(font, text, pos + new Vector2(-1,1), Color.Black);
+                spriteBatch.DrawString(font, text, pos + new Vector2(-1,-1), Color.Black);
+                spriteBatch.DrawString(font, text, pos, Color.White);
+        }
+
+        private void DrawHpBar(SpriteBatch spriteBatch)
+        {
+            int hpBarWidth = width * 2 / 3;
+            int hpBarHeight = 10;
+            int borderSize = 3;
+            Color bgColor = Color.Black;
+            Color fgColor = Color.Red;
+
+            spriteBatch.Draw(ContentLoader.CreateSolidtexture(bgColor), 
+                new Rectangle(
+                    (int)position.X + (width - hpBarWidth)/2 + 5,
+                    (int)position.Y, hpBarWidth, hpBarHeight), 
+                Color.White);
+
+            spriteBatch.Draw(ContentLoader.CreateSolidtexture(fgColor), 
+                new Rectangle(
+                    (int)position.X + (width - hpBarWidth)/2 + 5 + borderSize,
+                    (int)position.Y + borderSize,
+                    (hpBarWidth - 5 - borderSize) * currHealth / maxHealth,
+                    hpBarHeight - borderSize*2),
+                Color.White);
+            
+            var measure = font.MeasureString(currHealth.ToString()) + new Vector2(2,2);
+
+            spriteBatch.Draw(ContentLoader.CreateSolidtexture(bgColor), 
+                new Rectangle(
+                    (int)position.X + (width - hpBarWidth)/2 + 5,
+                    (int)position.Y,
+                    (int)measure.X + borderSize,
+                    (int)measure.Y + borderSize*2),
+                Color.White);
+
+            spriteBatch.Draw(ContentLoader.CreateSolidtexture(bgColor), 
+                new Rectangle(
+                    (int)position.X + (width - hpBarWidth)/2 + 5 + (int)measure.X + borderSize,
+                    (int)position.Y + hpBarHeight,
+                    borderSize, 
+                    (int)measure.Y - hpBarHeight + borderSize*2), 
+                Color.White);
+
+            spriteBatch.Draw(ContentLoader.CreateSolidtexture(fgColor), 
+                new Rectangle(
+                    (int)position.X + (width - hpBarWidth)/2 + 5 + borderSize,
+                    (int)position.Y + borderSize, 
+                    (int)measure.X,
+                    (int)measure.Y),
+                Color.White);
+
+            
+            DrawOutlinedString(spriteBatch, font, currHealth.ToString(), 
+                new Vector2(
+                    position.X + (width - hpBarWidth)/2 + 5 + borderSize + 1,
+                    position.Y + borderSize + 1),
+                Color.White);
         }
 
         public override void Update(GameTime gameTime)
--- src/SpaceVsInvaders/View/Board/TowerTile.cs
+++ src/SpaceVsInvaders/View/Board/TowerTile.cs
@@ -14,6 +14,7 @@ namespace SpaceVsInvaders.View.Boards
 
         private int currHealth;
         private int maxHealth;
+        private SpriteFont font;
 
 
         public TowerTile(Vector2 position, int height, int width, int row, int col, StateManager stateManager, TowerType tower, int currHealth, int maxHealth)
@@ -27,19 +28,92 @@ namespace SpaceVsInvaders.View.Boards
 
             this.currHealth = currHealth;
             this.maxHealth = maxHealth;
+
+            font = ContentLoader.GetFont("Fonts/NumberFont");
         }
 
         public override void Draw(SpriteBatch spriteBatch)
         {
             int size = (width > height) ? height : width;
 
+
             var rect = new Rectangle((int)position.X + (width - size)/2, (int)position.Y + (height - size)/2, size, size);
             spriteBatch.Draw(texture, rect, new Rectangle(0, 0, texture.Width, texture.Height), Color.White);
 
-            spriteBatch.DrawString(ContentLoader.GetFont("Fonts/NumberFont"), $"{currHealth}/{maxHealth}", new Vector2(position.X + 4, position.Y + height - 20), Color.White);
+            // spriteBatch.DrawString(ContentLoader.GetFont("Fonts/NumberFont"), $"{currHealth}/{maxHealth}", new Vector2(position.X + 4, position.Y + height - 20), Color.White);
+            DrawHpBar(spriteBatch);
            
            base.Draw(spriteBatch);
         }
+        
+        private void DrawOutlinedString(SpriteBatch spriteBatch, SpriteFont font, string text, Vector2 pos, Color color)
+        {
+                spriteBatch.DrawString(font, text, pos + new Vector2(1,1), Color.Black);
+                spriteBatch.DrawString(font, text, pos + new Vector2(1,-1), Color.Black);
+                spriteBatch.DrawString(font, text, pos + new Vector2(-1,1), Color.Black);
+                spriteBatch.DrawString(font, text, pos + new Vector2(-1,-1), Color.Black);
+                spriteBatch.DrawString(font, text, pos, Color.White);
+        }
+
+        private void DrawHpBar(SpriteBatch spriteBatch)
+        {
+            int hpBarWidth = width * 2 / 3;
+            int hpBarHeight = 10;
+            int borderSize = 3;
+            Color bgColor = Color.Black;
+            Color fgColor = Color.MediumSeaGreen;
+
+            spriteBatch.Draw(ContentLoader.CreateSolidtexture(bgColor), 
+                new Rectangle(
+                    (int)position.X + (width - hpBarWidth)/2 + 5, 
+                    (int)position.Y + height - hpBarHeight, 
+                    hpBarWidth, 
+                    hpBarHeight), 
+                Color.White);
+
+            spriteBatch.Draw(ContentLoader.CreateSolidtexture(fgColor), 
+                new Rectangle(
+                    (int)position.X + (width - hpBarWidth)/2 + 5 + borderSize, 
+                    (int)position.Y + height - hpBarHeight + borderSize, 
+                    (hpBarWidth - 5 - borderSize) * currHealth / maxHealth, 
+                    hpBarHeight - borderSize*2), 
+                Color.White);
+            
+            var measure = font.MeasureString(currHealth.ToString()) + new Vector2(2,2);
+
+            spriteBatch.Draw(ContentLoader.CreateSolidtexture(bgColor), 
+                new Rectangle(
+                    (int)position.X + (width - hpBarWidth)/2 + 5,
+                    (int)position.Y + height - (int)measure.Y - borderSize*2,
+                    (int)measure.X + borderSize,
+                    (int)measure.Y + borderSize*2),
+                Color.White);
+
+            spriteBatch.Draw(ContentLoader.CreateSolidtexture(bgColor), 
+                new Rectangle(
+                    (int)position.X + (width - hpBarWidth)/2 + 5 + borderSize + (int)measure.X,
+                    (int)position.Y + height - (int)measure.Y - borderSize*2,
+                    borderSize,
+                    (int)measure.Y - hpBarHeight + borderSize*2), 
+                Color.White);
+
+            spriteBatch.Draw(ContentLoader.CreateSolidtexture(fgColor), 
+                new Rectangle(
+                    (int)position.X + (width - hpBarWidth)/2 + 5 + borderSize,
+                    (int)position.Y + height - (int)measure.Y - borderSize,
+                    (int)measure.X,
+                    (int)measure.Y),
+                Color.White);
+
+            
+            DrawOutlinedString(spriteBatch, font, currHealth.ToString(), 
+                new Vector2(
+                    position.X + (width - hpBarWidth)/2 + 5 + borderSize + 1,
+                    position.Y + height - (int)measure.Y - 1),
+                Color.White);
+        }
+
+
 
         public override void Update(GameTime gameTime)
         {
